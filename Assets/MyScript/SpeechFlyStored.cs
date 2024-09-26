43179d83a5322f8fbddcfef3b0a95fcda58044f5
TiggerJeff3669 <winyes123@live.com>
2024-09-26 13:21:39 +0800

    configured fly map

---
+++ Assets/MyScript/SpeechFlyStored.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpeechFlyStored : MonoBehaviour
+{
+    // 101 25.033545724922448, 121.56480030166153
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
